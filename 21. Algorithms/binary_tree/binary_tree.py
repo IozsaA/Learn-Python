4a290404a28501731de4c31d8f0bdfa83309cd0f
MoNNsterFTW <iozsaandrei55@gmail.com>
2020-07-22 19:37:21 +0300

    commit

--- 21. Algorithms/binary_tree/binary_tree.py
+++ 21. Algorithms/binary_tree/binary_tree.py
@@ -22,7 +22,7 @@ class BinaryTree:
                 else:
                     current_node.right = new_node
                     break
-    
+
     def inorder(self):
         self._inorder_recursive(self.head)
 
@@ -39,7 +39,47 @@ class BinaryTree:
             if value == current_node.value:
                 return current_node
             elif value > current_node.value:
-                current_node= current_node.right
+                current_node = current_node.right
             else:
                 current_node = current_node.left
         raise LookupError(f'A node with value {value} was not found.')
+
+        def find_parent(self, value: int) -> Node:
+            if self.head and self.head.value == value:
+                return self.head
+            current_node = self.head
+            while current_node:
+                if(current_node.left and current_node.left.value == value) or\
+                        (current_node.right and current_node.right.value == value):
+                    return current_node
+                elif value > current_node.value:
+                    current_node = current_node.right
+                elif value < current_node.value:
+                    current_node = current_node.left
+
+        def find_rightmost(self, node: Node) -> Node:
+            current_node = node
+            while current_node.right:
+                current_node = current_node.right
+            return current_node
+
+        def delete(self, value: int):
+            to_delete = self.find(value)
+            to_delete_parent = self.find_parent(value)
+
+            if to_delete.left and to_delete.right:
+                pass
+            elif to_delete.left or to_delete.right:
+                if to_delete == to_delete_parent.left:
+                    to_delete_parent.left = to_delete.right or to_delete.left
+                elif to_delete == to_delete_parent.right:
+                    to_delete_parent.right = to_delete.right or to_delete.left
+                else:
+                    self.head= to_delete.right or to_delete.left
+            else:
+                if to_delete == to_delete_parent.left:
+                    to_delete_parent = None
+                elif to_delete == to_delete_parent.right:
+                    to_delete_parent.right = None
+                else:
+                    self.head = None
